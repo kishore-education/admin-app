5afcb38e1f4280659cb6b25e69867d5a3d8a5f09
kishore S <727823tucy023@skct.edu.in>
2024-09-30 19:46:45 +0530

    0.4

--- admin-app/main.py
+++ admin-app/main.py
@@ -112,6 +112,7 @@ hide_streamlit_style = """
     <style>
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
+    footer:after {content:''; visibility: hidden;}
     </style>
     """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
